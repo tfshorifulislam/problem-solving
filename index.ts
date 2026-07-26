096c69d3f4b3634652092b1c3ccefc7f39e291e8
tfshorifulislam <tfshorifulislam@gmail.com>
2026-07-27 02:34:14 +0600

    first problem solve

---
+++ index.ts
@@ -0,0 +1,19 @@
+// Variables, Data Types & Operators
+
+// const A: number = 5;
+// const B: number = 10;
+
+// let x: number = A;
+// const b: number = x;
+// const a: number = B
+
+// console.log(a, b)
+
+
+const A: number = 5;
+const B: number = 10;
+
+const a: number = B;
+const b: number = A;
+
+console.log(a, b); // 10 5
