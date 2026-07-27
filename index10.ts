e22368bf5c93b51790fc26d8c1ee8827c8238f01
tfshorifulislam <tfshorifulislam@gmail.com>
2026-07-27 11:59:24 +0600

    problem number 10 solve

---
+++ index10.ts
@@ -0,0 +1,18 @@
+
+
+const countChar = (str: string, char: string): number => {
+    let count = 0;
+
+    for (const letter of str) {
+        if (letter === char) {
+            count++
+        }
+    }
+
+    return count;
+}
+
+
+console.log(countChar("banana", "a"));
+console.log(countChar("banana", "n"));
+console.log(countChar("hello", "l"));
