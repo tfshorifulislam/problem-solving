8b37cd157a1b6155b8415b91a1ada1c8f0ea41f4
tfshorifulislam <tfshorifulislam@gmail.com>
2026-07-28 14:02:54 +0600

    problem number 11 solve

--- index11.ts
+++ index11.ts
@@ -1,10 +1,39 @@
 
 
-const sumArray = (arr: number[]): number => {
+// const sumArray = (arr: number[]): number => {
 
-    return arr.reduce((sum, num) => sum + num, 0);
+//     return arr.reduce((sum, num) => sum + num, 0);
 
+// }
+
+
+// const sumArry = (arr: number[]): number => {
+//     return arr.reduce((sum, num) => sum + num, 0)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const sumArry = (arr:number[]):number => {
+    return(
+        arr.reduce((a,b) => a + b, 0)
+    )
 }
 
-console.log(sumArray([1, 2, 3, 4, 5])); // 15
-console.log(sumArray([10, 20, 30]));    // 60
+console.log(sumArry([1, 2, 3, 4, 5]));
+console.log(sumArry([10, 20, 30]));
