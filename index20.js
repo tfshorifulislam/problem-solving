7ccc7511143eb41570d5960c9d3a169b4cdc6111
tfshorifulislam <tfshorifulislam@gmail.com>
2026-07-31 15:39:52 +0600

    20 number problem solve

---
+++ index20.js
@@ -0,0 +1,34 @@
+const input = [
+    { name: "Ali" },
+    { name: "Sara" },
+    { name: "Ali" }
+];
+
+
+const findDuplicateNames = (arr) => {
+
+    let count = {};
+    let result = [];
+
+    for (let person of arr) {
+        let name = person?.name;
+
+        if (count[name]) {
+            count[name]++;
+        }
+        else {
+            count[name] = 1;
+        }
+    }
+
+    for (let name in count) {
+        if (count[name] > 1) {
+            result.push(name)
+        }
+    }
+
+    return result;
+
+}
+
+console.log(findDuplicateNames(input));
