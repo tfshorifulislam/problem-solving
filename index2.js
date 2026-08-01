540ab0da501620b7c6d7b8348d283206b203145e
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-01 11:14:24 +0600

    checkEvenOdd problem solve

---
+++ index2.js
@@ -0,0 +1,31 @@
+
+// const checkEvenOdd = (n: number): boolean => {
+
+//     return n % 2 === 0
+
+// }
+
+
+
+
+
+
+
+
+
+
+// const checkEvenOdd = (n) => {
+//   if(n % 2 === 0) {
+//     return 'Even'
+//   }
+//   return 'Odd'
+// }
+
+
+
+const checkEvenOdd = (n) => n % 2 ===0 ? "Even" : "Odd"
+
+
+
+console.log(checkEvenOdd(4));
+console.log(checkEvenOdd(7));
--- index2.ts
+++
@@ -1,9 +0,0 @@
-
-const checkEvenOdd = (n: number): boolean => {
-
-    return n % 2 === 0
-
-}
-
-console.log(checkEvenOdd(4));
-console.log(checkEvenOdd(7));
