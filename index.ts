7789ea05c4ec2b6c06bd9976a9e08217e8838d50
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-01 11:09:26 +0600

    sawp problem solve

--- index.ts
+++ index.ts
@@ -10,10 +10,26 @@
 // console.log(a, b)
 
 
-const A: number = 5;
-const B: number = 10;
+// const A: number = 5;
+// const B: number = 10;
 
-const a: number = B;
-const b: number = A;
+// const a: number = B;
+// const b: number = A;
 
-console.log(a, b); // 10 5
+// console.log(a, b); // 10 5
+
+
+
+
+
+
+// Problem 1: Swap Two Variables  [Easy]
+// Description: Write a function that swaps the values of two variables without using a third variable.
+// Example:
+// Input: a = 5, b = 10Output: a = 10, b = 5
+// Hint: Try using destructuring or arithmetic operators.
+
+
+const sawp = (a, b) => [b, a];
+const [a, b] = sawp(5, 10);
+console.log(a, b); // Output: 10 5
--- index20.js
+++ index20.js
@@ -5,30 +5,83 @@ const input = [
 ];
 
 
+// const findDuplicateNames = (arr) => {
+
+//     let count = {};
+//     let result = [];
+
+//     for (let person of arr) {
+//         let name = person?.name;
+
+//         if (count[name]) {
+//             count[name]++;
+//         }
+//         else {
+//             count[name] = 1;
+//         }
+//     }
+
+//     for (let name in count) {
+//         if (count[name] > 1) {
+//             result.push(name)
+//         }
+//     }
+
+//     return result;
+
+// }
+
+
+// const findDuplicateNames = (arr) => {
+
+//     let count = {};
+//     let result = [];
+
+//     for (let person of arr) {
+//         let name = person.name;
+
+//         if (count[name]) {
+//             count[name]++
+//         }
+//         else {
+//             count[name] = 1;
+//         }
+//     }
+
+//     for (let name in count) {
+//         if (count[name] > 1) {
+//             result.push(name)
+//         }
+//     }
+
+//     return result;
+// }
+
+
 const findDuplicateNames = (arr) => {
 
-    let count = {};
+    let count = {}
     let result = [];
 
     for (let person of arr) {
-        let name = person?.name;
+        let name = person.name;
 
         if (count[name]) {
-            count[name]++;
+            count[name]++
         }
         else {
             count[name] = 1;
         }
     }
 
-    for (let name in count) {
+    for (name in count) {
         if (count[name] > 1) {
             result.push(name)
         }
     }
 
     return result;
-
 }
+
 
 console.log(findDuplicateNames(input));
