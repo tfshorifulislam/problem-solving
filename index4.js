06968c4899ee8b63186b4d211b7f2b35b68b6570
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-01 11:51:35 +0600

    toFahrenheit problem solve

---
+++ index4.js
@@ -0,0 +1,27 @@
+
+
+// const toFahrenheit = (celsius: number): number => {
+//     return (
+//         celsius * 9 / 5 + 32
+//     )
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const toFahrenheit = (c) => {
+    return c* 9/5 +32
+}
+console.log(toFahrenheit(0))
+console.log(toFahrenheit(100))
