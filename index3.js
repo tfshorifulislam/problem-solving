14cd5801850a51934f20f1bf60d459c44f128393
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-01 11:18:29 +0600

    largestNumber problem solve with math.max

---
+++ index3.js
@@ -0,0 +1,38 @@
+
+
+// const largest = (a: number, b: number, c: number): number => {
+//     return Math.max(a, b, c)
+// }
+
+// console.log(largest(3, 7, 5));
+// console.log(largest(50, 5, 30));
+
+
+// const largestNumber = (a: number, b: number, c: number): number => {
+//     if (a > b && a > c) {
+//         return a;
+//     }
+
+//     else if (b > a && b > c) {
+//         return b;
+//     }
+
+//     else {
+//         return c
+//     }
+// }
+
+
+
+
+
+
+// const largestNumber = (a , b, c) => {
+//     return Math.max(a, b, c)
+// }
+
+
+
+
+console.log(largestNumber(3, 7, 5));  
+console.log(largestNumber(50, 5, 30));
