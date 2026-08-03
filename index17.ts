b1e4dd6db537bbeffcbdd06856df44f0d809a785
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-03 23:16:24 +0600

    mergeObjects problem solve

--- index17.ts
+++ index17.ts
@@ -1,7 +1,23 @@
 
 
-const mergeObjects = (obj1, obj2) => {
-    return({...obj1, ...obj2})
+// const mergeObjects = (obj1, obj2) => {
+//     return({...obj1, ...obj2})
+// }
+
+
+
+
+
+
+
+
+
+
+
+const mergeObjects = (object1: Record<string, unknown>, object2: Record<string, unknown>) => {
+    return({...object1, ...object2})
 }
+
+
 
 console.log(mergeObjects({ a: 1 }, { b: 2 }));
