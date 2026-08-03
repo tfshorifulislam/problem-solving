0cd797b0a3490cdd449d4f561c9bd8de59fbe7c9
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-03 23:18:19 +0600

    flattenArray problem solve

--- index14.ts
+++ index14.ts
@@ -1,7 +1,19 @@
 
 
-const flattenArray = (arr:number[]):number[] => {
-    return arr.flat()
+// const flattenArray = (arr:number[]):number[] => {
+//     return arr.flat()
+// }
+
+
+
+
+
+
+
+
+const flattenArray = (array:number[]):number[] => {
+    return array.flat()
 }
+
 
 console.log(flattenArray([1, [2, 3], [4, 5]]));
