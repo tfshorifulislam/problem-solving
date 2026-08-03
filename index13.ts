0330dc65eafed651b900e146d467d628341eefe4
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-03 23:19:38 +0600

    removeDuplicates problem solve

--- index13.ts
+++ index13.ts
@@ -9,11 +9,26 @@
 
 
 
-const removeDuplicates = (arr: number[]): number[] => {
+// const removeDuplicates = (arr: number[]): number[] => {
+//     return (
+//         arr.filter((item, index) => arr.indexOf(item) === index)
+//     )
+// }
+
+
+
+
+
+
+
+const removeDuplicates = (array: number[]): number[] => {
     return (
-        arr.filter((item, index) => arr.indexOf(item) === index)
+        array.filter((item, index) => array.indexOf(item) === index)
     )
 }
+
+
+
 
 console.log(removeDuplicates([1, 2, 2, 3, 3, 4]));
 // Output: [1, 2, 3, 4]
