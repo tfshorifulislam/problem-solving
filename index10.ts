81cd5abd41b5e47e02b19764379932f86b5d2f19
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-03 23:22:15 +0600

    countChar problem solve

--- index10.ts
+++ index10.ts
@@ -1,9 +1,24 @@
 
 
-const countChar = (str: string, char: string): number => {
+// const countChar = (str: string, char: string): number => {
+//     let count = 0;
+
+//     for (const letter of str) {
+//         if (letter === char) {
+//             count++
+//         }
+//     }
+
+//     return count;
+// }
+
+
+
+
+const countChar = (string: string, char: string): number => {
     let count = 0;
 
-    for (const letter of str) {
+    for (const letter of string) {
         if (letter === char) {
             count++
         }
@@ -11,7 +26,6 @@ const countChar = (str: string, char: string): number => {
 
     return count;
 }
-
 
 console.log(countChar("banana", "a"));
 console.log(countChar("banana", "n"));
