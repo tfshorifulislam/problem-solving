78497ed7c86abfa04c35cab9929c322cf42d3125
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-03 23:15:28 +0600

    fizzBuzz problem solve

--- index18.ts
+++ index18.ts
@@ -1,7 +1,25 @@
 
 
-const fizzBuzz = (n) => {
-    for (let i = 1; i <= n; i++) {
+// const fizzBuzz = (n) => {
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 15 === 0) {
+//             console.log('FizzBuzz')
+//         }
+//         else if (i % 3 === 0) {
+//             console.log('Fizz')
+//         }
+//         else if (i % 5 === 0) {
+//             console.log("Buzz")
+//         }
+//         else {
+//             console.log(i)
+//         }
+//     }
+// }
+
+
+const fizzBuzz = (number:number) => {
+    for (let i = 1; i <= number; i++) {
         if (i % 15 === 0) {
             console.log('FizzBuzz')
         }
