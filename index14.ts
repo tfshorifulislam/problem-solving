fbc9e646401573c167abaedc931d9eafc5be8f97
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-27 22:29:05 +0600

    flatten array problem solve in for of loop use

--- index14.ts
+++ index14.ts
@@ -16,10 +16,24 @@
 // }
 
 
+// const flattenArray = (array: number[][]): number[] => {
+//     return array.reduce((result, current) => {
+//         return result.concat(current);
+//     }, []);
+// };
+
+
+
 const flattenArray = (array: number[][]): number[] => {
-    return array.reduce((result, current) => {
-        return result.concat(current);
-    }, []);
+    const result: number[] = [];
+
+    for (const item of array) {
+        for (const num of item) {
+            result.push(num);
+        }
+    }
+
+    return result;
 };
 
 console.log(flattenArray([[1, 2], [3, 4], [5, 6]]));
