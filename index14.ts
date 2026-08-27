fb2fb19dbee5deb96b603dd4c6ba2c9d9e6be225
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-27 22:28:16 +0600

    flatten an arry with reduce systme it is a problem solve system

--- index14.ts
+++ index14.ts
@@ -11,9 +11,18 @@
 
 
 
-const flattenArray = (array:number[]):number[] => {
-    return array.flat()
-}
+// const flattenArray = (array:number[]):number[] => {
+//     return array.flat()
+// }
 
+
+const flattenArray = (array: number[][]): number[] => {
+    return array.reduce((result, current) => {
+        return result.concat(current);
+    }, []);
+};
+
+console.log(flattenArray([[1, 2], [3, 4], [5, 6]]));
+// [1, 2, 3, 4, 5, 6]
 
 console.log(flattenArray([1, [2, 3], [4, 5]]));
