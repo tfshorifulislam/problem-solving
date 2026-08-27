53eca67839d2c1a8852b7232c56922cf021d51e8
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-27 22:23:00 +0600

    problem solve using by forEach

--- index11.ts
+++ index11.ts
@@ -61,12 +61,25 @@
 // };
 
 
+// const sumArray = (array: number[]): number => {
+//     let sum = 0;
+
+//     for (let i = 0; i < array.length; i++) {
+//         sum += array[i];
+//     }
+
+//     return sum;
+// };
+
+
+
+
 const sumArray = (array: number[]): number => {
     let sum = 0;
 
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i];
-    }
+    array.forEach((number) => {
+        sum += number;
+    });
 
     return sum;
 };
